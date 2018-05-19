58cd633cc745615c8754296843d1d150d5932a0d
GreenyRepublic <benfclark@urog.co.uk>
2018-05-19 16:16:54 +0100

    Revert "meshes"
    This reverts commit c544f31631bf3820e377bd05b60bda9491708d38.

---
+++ external/CGAL_4_12/lib/cmake/CGAL/CGALExports-relwithdebinfo.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "RelWithDebInfo".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "CGAL::CGAL" for configuration "RelWithDebInfo"
+set_property(TARGET CGAL::CGAL APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(CGAL::CGAL PROPERTIES
+  IMPORTED_IMPLIB_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/CGAL-vc140-mt-4.12.lib"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/bin/CGAL-vc140-mt-4.12.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS CGAL::CGAL )
+list(APPEND _IMPORT_CHECK_FILES_FOR_CGAL::CGAL "${_IMPORT_PREFIX}/lib/CGAL-vc140-mt-4.12.lib" "${_IMPORT_PREFIX}/bin/CGAL-vc140-mt-4.12.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
